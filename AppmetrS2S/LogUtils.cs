93d66ed11a030e1f970075166335987e25468cc8
M729\b.nichvolodov <nichvolodov.boris@gmail.com>
2016-09-19 13:51:41 +0300

    support custom logger, fix serialization

---
+++ AppmetrS2S/LogUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using log4net;
+
+namespace AppmetrS2S
+{
+    public static class LogUtils
+    {
+        public static ILog CustomLog { get; set; }
+
+        public static ILog GetLogger(Type type)
+        {
+            if (CustomLog != null)
+                return CustomLog;
+
+            return LogManager.GetLogger(type);
+        }
+    }
+}
