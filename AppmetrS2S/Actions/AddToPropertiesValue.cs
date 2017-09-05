81fdf43b4ca87bfcbc97febe80d9664d09fa3421
Stanislav Yablonskiy <s.yablonskiy@pixonic.ru>
2017-09-05 11:16:13 +0300

    AddToPropertiesValue action added

---
+++ AppmetrS2S/Actions/AddToPropertiesValue.cs
@@ -0,0 +1,19 @@
+﻿namespace AppmetrS2S.Actions
+{
+    #region using directives
+
+    using System;
+    using System.Runtime.Serialization;
+
+    #endregion
+
+    [DataContract]
+    public class AddToPropertiesValue : AppMetrAction
+    {
+        private const String ACTION = "addToPropertiesValue";
+
+        public AddToPropertiesValue() : base(ACTION)
+        {
+        }
+    }
+}
