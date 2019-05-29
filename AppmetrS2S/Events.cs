307110a56eba4f81ab5e3f702c5f1117d43f7428
Tomas Shestakov <t.shestakov@pixonic.com>
2019-05-29 10:46:48 +0300

    added Events to project

---
+++ AppmetrS2S/Events.cs
@@ -0,0 +1,17 @@
+﻿namespace AppmetrS2S.Actions
+{
+    #region using directives
+
+    using System;
+
+    #endregion
+    public static class Events
+    {
+        public static Event ServerInstall(String userId)
+        {
+            Event _event = new Event("server/server_install");
+            _event.SetUserId(userId);
+            return _event;
+        }
+    }
+}
