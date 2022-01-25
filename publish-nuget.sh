8f955081d3a031751b24dfa21fed35b8d6ecd80b
Stanislav Yablonskiy <s.yablonskiy@pixonic.com>
2022-01-25 16:26:10 +0300

    Revert "NuGet publish script added <META-139>"
    This reverts commit 9d1dad1ac7320167bc90791a0ca6909d776de2c5.

--- publish-nuget.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-dotnet pack -p:TargetFrameworks=netstandard2.0 -c:Release AppMetrS2S/AppmetrS2S.csproj
-dotnet nuget push AppmetrS2S/bin/Release/AppmetrS2S.*.nupkg -s https://nexus.pixapi.net/repository/nuget-hosted/ -k $NEXUS_NUGET_API_KEY
